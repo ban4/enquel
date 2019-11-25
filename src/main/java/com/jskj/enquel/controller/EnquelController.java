7eb87b804bc4bb4ba1a3b94683ca899a8bd4e361
anbaihong <anbaihong@nequal.com>
2019-11-25 10:20:47 +0800

    feat(module):add delete method 添加删除方法，根据id删除某个信息；修改查询请求url

--- src/main/java/com/jskj/enquel/controller/EnquelController.java
+++ src/main/java/com/jskj/enquel/controller/EnquelController.java
@@ -1,6 +1,7 @@
 package com.jskj.enquel.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,24 @@ public class EnquelController {
     @Value("${enquel.age}")
     private String age;
 
-    @GetMapping("/getInfo/{id}")
+    @GetMapping("/{id}")
     public String getInfo(@PathVariable int id){
+        if (1 < id) {
+            return "查询失败";
+        }else {
+            return "name: " + name + " --- age: " + age;
+        }
+    }
 
-        return "查询失败";
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable int id){
+
+        if (1 == id){
+            return "删除成功";
+        }else {
+            return "删除失败";
+        }
+
     }
 
 }
