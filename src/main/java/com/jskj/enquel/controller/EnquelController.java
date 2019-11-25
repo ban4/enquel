81003f1595ed30e731758c7db6133a3f02bde88b
anbaihong <anbaihong@nequal.com>
2019-11-25 11:10:52 +0800

    fix(module):update get method

--- src/main/java/com/jskj/enquel/controller/EnquelController.java
+++ src/main/java/com/jskj/enquel/controller/EnquelController.java
@@ -1,6 +1,10 @@
 package com.jskj.enquel.controller;
 
+import com.jskj.enquel.entity.UserDTO;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +36,21 @@ public class EnquelController {
         }else {
             return "删除失败";
         }
+
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable int id){
+        if (1 == id) {
+            UserDTO userDTO = new UserDTO("enquel","11");
+            HttpHeaders responseHeaders = new HttpHeaders();
+            responseHeaders.set("X-Total-Count", String.valueOf(199));
+            return new ResponseEntity<>(userDTO, responseHeaders, HttpStatus.OK);
+        }else {
+            UserDTO userDTO = new UserDTO("no","0");
+            return new ResponseEntity<>(userDTO, new HttpHeaders(), HttpStatus.OK);
+        }
+
 
     }
 
